5fc55fd615de78136a8b334a7cae2a13572c8bef
Gonzalo.Gonzalez.A <gmx.arostegui@gmail.com>
2025-11-27 22:12:09 -0300

    hito 6.3

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router";
-import ContextProvider from "./context/ContextProvider";
+import { ContextProvider } from "./context/ContextProvider";
 
 
 createRoot(document.getElementById("root")).render(
